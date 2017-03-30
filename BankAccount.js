2215f1458e383de029691826d158bca3fb4396f4
Bill Ivan <billivank_96@yahoo.co.id>
2017-03-30 10:19:57 +0700

    public private interface

--- BankAccount.js
+++ BankAccount.js
@@ -4,19 +4,21 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor (customer_name, type, acct_number) {
-    //
+    this._customer_name = customer_name;
+    this._type = type;
+    this._acct_number = acct_number
   }
 
   get account_number () {
-    //
+    return this._acct_number
   }
 
   to_string () {
-    //
+    console.log(`${this._customer_name}: ${this._type}# ${this._acct_number}`)
   }
 
   cover_digits () {
-    //
+    console.log(`${this._customer_name}: ${this._type}# ***-***-${this._acct_number.slice(8,11)}`)
   }
 }
 
