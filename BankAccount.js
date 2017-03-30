6b8a61a6ba3cbf388b02940183b41b2557314c45
ambo-dalle <ambodalle.st@gmail.com>
2017-03-30 17:10:24 +0700

    Selesai

--- BankAccount.js
+++ BankAccount.js
@@ -4,25 +4,31 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor (customer_name, type, acct_number) {
-    //
+     this._customer_name = customer_name;
+     this._type          = type;
+     this._acct_number   = acct_number;
   }
 
   get account_number () {
-    //
+    return this._acct_number;
   }
 
   to_string () {
-    //
+    //console.log(this._customer_name+ " : " +this._type+ "# " +this._acct_number);
+    console.log(`${this._customer_name} : ${this._type} # ${this._acct_number}`);
   }
 
   cover_digits () {
-    //
+    let regex = /(\d{3}[-]\d{3}[-])/g;
+    let coverDigit = this._acct_number.replace(regex,"***-***-");
+    //console.log(+this._customer_name+ " : " +this._type+ "# " +coverDigit);
+    console.log(`${this._customer_name} : ${this._type} # ${coverDigit}`);
   }
 }
 
 let my_acct = new BankAccount('Hacktivate', 'Checking', '333-555-888')
 
-console.log(my_acct)
+//console.log(my_acct)
 console.log(my_acct.account_number)
 
 // release 0
