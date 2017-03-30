f8fd784649afa4e7e31fa8ada47783ee2d47d8c5
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-03-30 11:41:49 +0700

    Komit1

--- BankAccount.js
+++ BankAccount.js
@@ -4,21 +4,30 @@ const ACCT_NUMBER = new WeakMap()
 
 class BankAccount {
   constructor (customer_name, type, acct_number) {
-    //
+    this.customer_name = customer_name;
+    this.type = type;
+    this._acct_number = acct_number;
   }
 
   get account_number () {
-    //
+    return this._acct_number;
   }
 
   to_string () {
-    //
+     console.log("Hacktivate: Checking# " + this._acct_number);
   }
 
+
   cover_digits () {
-    //
+      var pisahsetrip = this._acct_number.split("-");
+      for (var i = 0; i < pisahsetrip.length - 1; i++){
+        pisahsetrip[i] = "***";
+      }
+      var hidden = pisahsetrip.join("-");
+      console.log(hidden);
+      }
   }
-}
+
 
 let my_acct = new BankAccount('Hacktivate', 'Checking', '333-555-888')
 
